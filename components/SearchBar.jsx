8ae2370263c4a0f2b725bfb9bbce5b0ec9933d3e
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2021-01-03 01:00:14 -0500

    added two event handlers to search bar, onChange and onClick

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { term: 'Begin Search'.toUpperCase() }
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div>
-              <label >Image Search</label>
-            <input type="text" />
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={event => this.setState({ term: event.target.value })}
+              onClick={event => this.setState({ term: event.target.value = '' })}
+            />
           </div>
         </form>
       </div>
